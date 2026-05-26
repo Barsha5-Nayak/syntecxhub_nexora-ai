0eb6c57b7ebd25c87802bc6acbd38a768308b9f0
Barsha5-Nayak <nayakbarshanayk@gmail.com>
2026-05-26 20:20:28 +0530

    mobile responsive fixed

---
+++ script.js
@@ -0,0 +1,57 @@
+const menuToggle = document.getElementById("menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function () {
+
+  navLinks.classList.toggle("active");
+
+});
+
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach(function(item){
+
+  item.addEventListener("click", function(){
+
+    navLinks.classList.remove("active");
+
+  });
+
+});
+
+
+
+/* TYPING EFFECT */
+
+const title = document.getElementById("typing-title");
+
+
+
+const titleText = "Future of AI Experience";
+
+
+
+
+
+let i = 0;
+
+function typeTitle(){
+
+  if(i < titleText.length){
+
+    title.innerHTML += titleText.charAt(i);
+
+    i++;
+
+    setTimeout(typeTitle, 80);
+
+  }
+
+}
+
+window.onload = typeTitle();
+
+
+
