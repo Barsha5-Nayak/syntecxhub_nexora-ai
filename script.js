3a7b4d9b88c4c29024397ac43929419ca414b635
Barsha5-Nayak <nayakbarshanayk@gmail.com>
2026-05-27 20:54:28 +0530

    fixed typing effect

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-<<<<<<< HEAD
-const menuToggle = document.getElementById("menu-toggle");
 
+const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
+/* MOBILE MENU */
+
 menuToggle.addEventListener("click", function () {
-
   navLinks.classList.toggle("active");
-
 });
-
 
 const navItems = document.querySelectorAll(".nav-links a");
 
 navItems.forEach(function(item){
 
   item.addEventListener("click", function(){
-
     navLinks.classList.remove("active");
-
   });
 
 });
-
 
 
 /* TYPING EFFECT */
 
 const title = document.getElementById("typing-title");
 
-
-
 const titleText = "Future of AI Experience";
-
-
-
-
 
 let i = 0;
 
@@ -47,71 +36,10 @@ function typeTitle(){
     i++;
 
     setTimeout(typeTitle, 80);
-
   }
 
 }
 
-window.onload = typeTitle();
+/* PAGE LOAD */
 
-
-
-=======
-const menuToggle = document.getElementById("menu-toggle");
-
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", function () {
-
-  navLinks.classList.toggle("active");
-
-});
-
-
-const navItems = document.querySelectorAll(".nav-links a");
-
-navItems.forEach(function(item){
-
-  item.addEventListener("click", function(){
-
-    navLinks.classList.remove("active");
-
-  });
-
-});
-
-
-
-/* TYPING EFFECT */
-
-const title = document.getElementById("typing-title");
-
-
-
-const titleText = "Future of AI Experience";
-
-
-
-
-
-let i = 0;
-
-function typeTitle(){
-
-  if(i < titleText.length){
-
-    title.innerHTML += titleText.charAt(i);
-
-    i++;
-
-    setTimeout(typeTitle, 80);
-
-  }
-
-}
-
-window.onload = typeTitle();
-
-
-
->>>>>>> fa9c35069be347df2f938e861a3d49c4fcc037ea
+window.onload = typeTitle;
